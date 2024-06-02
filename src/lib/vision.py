c62dd8f2589f0934e88d91b7e9bfd68d8b1fdf32
gksdud6330 <hy12500@korea.ac.kr>
2024-06-03 01:27:41 +0200

    Revert "Revert "State machine #1 updated""
    This reverts commit c0ec3d8020e4a83031bc8d275d8e61ef0dbcaf79.

--- src/lib/vision.py
+++ src/lib/vision.py
@@ -9,3 +9,11 @@ class Vision(ABC):
     @abstractmethod
     def get_frame(self) -> npt.NDArray[uint8]:
         """Get an RGB image frame from vision sensor (camera). Returns numpy image data."""
+
+    @abstractmethod
+    def detect_box(self):
+        """Check if the robot has arrived to the target."""
+
+    @abstractmethod
+    def detect_dest(self):
+        """Check if the robot has arrived to the target."""
